65449bde0f5affa8abe4bf9724c76180ccc5070b
AjayNaik0809 <ajaynaik8497@gmail.com>
2021-05-21 13:45:41 +0530

    dot Emp App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  const url = 'https://yza2oma942.execute-api.eu-west-1.amazonaws.com/Prod/api/officeData'
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get(url).then(json => setData(json.data))
+  }, [])
+
+  const renderTable = () => {
+    return data.map(user => {
+      return (
+        <tr alig>
+          <td>{user.sOfficeName}</td>
+          <td>{user.sOfficeAdd}</td>          
+        </tr>
+      )
+    })
+  }
+
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit1  <code>src/App.js</code> and save to reload.
         </p>
+
+        <p>
+        <h1 id="title"> Table</h1><table id="users"> 
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>{renderTable()}</tbody>
+      </table></p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,8 +51,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+        
         </a>
       </header>
+      
+      
     </div>
   );
 }

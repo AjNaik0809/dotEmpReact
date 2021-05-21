97e0c7cbbbf082b1e05f63ad58a7c9b760d49759
AjayNaik0809 <ajaynaik8497@gmail.com>
2021-05-21 15:08:52 +0530

    dot Emp App

--- src/App.js
+++ src/App.js
@@ -4,24 +4,77 @@ import axios from 'axios'
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const url = 'https://yza2oma942.execute-api.eu-west-1.amazonaws.com/Prod/api/officeData'
+  const urlGetData = 'https://yza2oma942.execute-api.eu-west-1.amazonaws.com/Prod/api/officeData'
+  const urlPostData = 'https://yza2oma942.execute-api.eu-west-1.amazonaws.com/Prod/api/officeData'
 
   const [data, setData] = useState([])
+  const [Office_Name, setOffice_Name] = useState('')
+  const [Office_Add, setOffice_Add] = useState('')
 
   useEffect(() => {
-   axios.get(url).then(json => setData(json.data))
+   axios.get(urlGetData).then(json => setData(json.data))
   }, [])
 
   const renderTable = () => {
     return data.map(user => {
       return (
-        <tr alig>
+        <tr >
           <td>{user.sOfficeName}</td>
           <td>{user.sOfficeAdd}</td>          
         </tr>
+
       )
     })
   }
+
+  function handleSubmit() {
+      console.log(Office_Name + " " + Office_Add)
+
+    axios({
+    method: 'post',
+    url: 'https://yza2oma942.execute-api.eu-west-1.amazonaws.com/Prod/api/officeData',
+    headers: {
+        'Content-Type': 'application/json'
+             },
+        data: {
+            sOfficeName: Office_Name,
+            sOfficeAdd: Office_Add           
+
+    }
+}).then(response => {
+  fetchAllData();
+    
+    })
+    .catch(error => {
+        console.log(error)
+    });
+  }
+
+function fetchAllData()
+{
+  axios.get(urlGetData).then(json => setData(json.data))
+}
+
+  const renderAddNewRow = () => {    
+      return (
+        <tr >
+          <td><input type="text" onChange={e => updateOfficeName(e.target.value)} /></td>
+          <td><input type="text" onChange={e => updateOfficeAdd(e.target.value)} /></td> 
+          <td><button value={"example"} onClick={handleSubmit} > Save </button> </td>         
+        </tr>
+      )
+  }
+
+  function updateOfficeName(eventvalue)
+  {
+    setOffice_Name(eventvalue);
+  }
+
+  function updateOfficeAdd  (eventvalue)
+  {
+    setOffice_Add(eventvalue);
+  }
+
 
   return (
     <div className="App">
@@ -38,7 +91,10 @@ function App() {
             <th>Address</th>
           </tr>
         </thead>
-        <tbody>{renderTable()}</tbody>
+        <tbody>          
+          {renderTable()}
+          {renderAddNewRow()}
+          </tbody>
       </table></p>
         <a
           className="App-link"
